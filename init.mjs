ab3d43452d427db10eadf6b17cb85c2ca368e46e
alenzer <alenzer0902@gmail.com>
2022-10-11 19:58:36 -0400

    deploy

--- init.mjs
+++ init.mjs
@@ -1,9 +1,9 @@
 import { ethers } from "ethers";
 import fs from "fs";
-
-const WEFUND_ABI = JSON.parse(fs.readFileSync("./build/WeFund.json"));
 // require("dotenv").config();
 import dotenv from "dotenv";
+
+const WEFUND_ABI = JSON.parse(fs.readFileSync("./build/WeFund.json"));
 dotenv.config();
 
 export const CHAINS_CONFIG = {
@@ -38,72 +38,66 @@ const pk = process.env.PK;
 
 async function main() {
   const WEFUND_CONTRACT = "0x003F0Cae69d7a28Bf9157c8cC27D466e00cA965e";
-  const provider = new ethers.providers.JsonRpcProvider(
-    CHAINS_CONFIG.rinkeby.rpc
-  );
+  const provider = new ethers.providers.JsonRpcProvider(CHAINS_CONFIG.rinkeby.rpc);
   if (!pk) return;
   const signer = new ethers.Wallet(pk, provider);
   const contract = new ethers.Contract(WEFUND_CONTRACT, WEFUND_ABI.abi, signer);
 
   let res;
-  // res = await contract.setTokenAddress(
-  //   "0xd92e713d051c37ebb2561803a3b5fbabc4962431",
-  //   "0x6ee856ae55b6e1a249f04cd3b947141bc146273c",
-  //   "0x16c550a97ad2ae12c0c8cf1cc3f8db4e0c45238f"
-  // );
-  // console.log(res);
-  // await res.wait();
+  res = await contract.setTokenAddress(
+    "0xd92e713d051c37ebb2561803a3b5fbabc4962431",
+    "0x6ee856ae55b6e1a249f04cd3b947141bc146273c",
+    "0x16c550a97ad2ae12c0c8cf1cc3f8db4e0c45238f"
+  );
+  console.log(res);
+  await res.wait();
 
-  // res = await contract.getProjectInfo();
-  // console.log(res);
+  res = await contract.getProjectInfo();
+  console.log(res);
 
-  // res = await contract.initialize();
-  // console.log(res);
+  res = await contract.initialize();
+  console.log(res);
 
-  // res = await contract.addProject(600000, [
-  //   [0, "1", "", "2022-03-1", "2022-03-31", "600000", "0", []],
-  // ]);
-  // console.log("1");
+  res = await contract.addProject(600000, [
+    [0, "1", "", "2022-03-1", "2022-03-31", "600000", "0", []],
+  ]);
+  console.log("1");
 
-  // res = await contract.addProject(390000, [
-  //   [0, "1", "", "2022-03-1", "2022-03-31", "390000", "0", []],
-  // ]);
-  // await res.wait();
-  // console.log("2");
-
-  // res = await contract.addProject(250000, [
-  //   [0, "1", "", "2022-03-1", "2022-03-31", "250000", "0", []],
-  // ]);
-  // await res.wait();
-  // console.log("3");
-
-  // res = await contract.addProject(600000, [
-  //   [0, "1", "", "2022-03-1", "2022-03-31", "600000", "0", []],
-  // ]);
-  // await res.wait();
-  // console.log("4");
-
-  // res = await contract.addProject(120000, [
-  //   [0, "1", "", "2022-03-1", "2022-03-31", "120000", "0", []],
-  // ]);
-  // await res.wait();
-  // console.log("5");
-
-  // res = await contract.addProject(120000, [
-  //   [0, "1", "", "2022-03-1", "2022-03-31", "120000", "0", []],
-  // ]);
-  // await res.wait();
-  // console.log("6");
-
-  res = await contract.addProject(4080000, [
-    [0, "1", "", "2022-03-1", "2022-03-31", "4080000", "0", []],
+  res = await contract.addProject(390000, [
+    [0, "1", "", "2022-03-1", "2022-03-31", "390000", "0", []],
   ]);
   await res.wait();
-  console.log("7");
+  console.log("2");
+
+  res = await contract.addProject(250000, [
+    [0, "1", "", "2022-03-1", "2022-03-31", "250000", "0", []],
+  ]);
+  await res.wait();
+  console.log("3");
+
+  res = await contract.addProject(600000, [
+    [0, "1", "", "2022-03-1", "2022-03-31", "600000", "0", []],
+  ]);
+  await res.wait();
+  console.log("4");
 
   res = await contract.addProject(120000, [
     [0, "1", "", "2022-03-1", "2022-03-31", "120000", "0", []],
   ]);
+  await res.wait();
+  console.log("5");
+
+  res = await contract.addProject(120000, [
+    [0, "1", "", "2022-03-1", "2022-03-31", "120000", "0", []],
+  ]);
+  await res.wait();
+  console.log("6");
+
+  res = await contract.addProject(4080000, [[0, "1", "", "2022-03-1", "2022-03-31", "4080000", "0", []]]);
+  await res.wait();
+  console.log("7");
+
+  res = await contract.addProject(120000, [[0, "1", "", "2022-03-1", "2022-03-31", "120000", "0", []]]);
   await res.wait();
   console.log("8");
 }
